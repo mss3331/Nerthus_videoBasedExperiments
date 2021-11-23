43cc33b4989d86ac80a3ffe4ad209f028714b608
Mahmood_Haithami <mss3331@hotmail.com>
2021-11-23 17:15:45 +0800

    - ResNet_subVideo_MLP have been added

--- models/fixed_subVideoLenght_models.py
+++ models/fixed_subVideoLenght_models.py
@@ -1,6 +1,141 @@
 import torchvision.models as models
 import torch
 import torch.nn as nn
+from .MLP_Mixer import MlpBlock, MixerBlock
+
+class Mlp(nn.Module):
+    """Entire network.
+
+    Parameters
+    ----------
+    image_size : int
+        Height and width (assuming it is a square) of the input image.
+
+    patch_size : int
+        Height and width (assuming it is a square) of the patches. Note
+        that we assume that `image_size % patch_size == 0`.
+
+    tokens_mlp_dim : int
+        Hidden dimension for the `MlpBlock` when doing the token mixing.
+
+    channels_mlp_dim : int
+        Hidden dimension for the `MlpBlock` when diong the channel mixing.
+
+    n_classes : int
+        Number of classes for classification.
+
+    hidden_dim : int
+        Dimensionality of patch embeddings.
+
+    n_blocks : int
+        The number of `MixerBlock`s in the architecture.
+
+    Attributes
+    ----------
+    patch_embedder : nn.Conv2D
+        Splits the image up into multiple patches and then embeds each of them
+        (using shared weights).
+
+    blocks : nn.ModuleList
+        List of `MixerBlock` instances.
+
+    pre_head_norm : nn.LayerNorm
+        Layer normalization applied just before the classification head.
+
+    head_classifier : nn.Linear
+        The classification head.
+    """
+
+    def __init__(self, *,n_frames, encoder_features , tokens_mlp_dim=None, channels_mlp_dim=None,
+                 n_blocks=5):
+        # number of patches is equivalant to number of images if applied for a video
+        super().__init__()
+        # n_patches = (image_size // patch_size) ** 2 # assumes divisibility
+        self.n_patches = n_frames # this is the maximium number of images for a subvideo
+        hidden_dim = encoder_features #number of features that ResNet50 provide
+        # No need for embeding since we have an Encoder (ResNet18)
+        # self.patch_embedder = nn.Conv2d(
+        #     3,
+        #     hidden_dim,
+        #     kernel_size=patch_size,
+        #     stride=patch_size,
+        # )
+
+        self.blocks = nn.ModuleList(
+            [
+                MixerBlock(
+                    n_patches=self.n_patches,
+                    hidden_dim=hidden_dim,
+                    tokens_mlp_dim=tokens_mlp_dim,
+                    channels_mlp_dim=channels_mlp_dim,
+                )
+                for _ in range(n_blocks)
+            ]
+        )
+
+        self.pre_head_norm = nn.LayerNorm(hidden_dim)
+        # self.head_classifier = nn.Linear(hidden_dim, n_classes)
+
+    def forward(self, x):
+        """Run the forward pass.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input batch of square images of shape
+            `(n_samples, n_channels, image_size, image_size)`.
+
+        Returns
+        -------
+        torch.Tensor
+            Class logits of shape `(n_samples, n_classes)`.
+        """
+        # no need for embedder and re arrange since our encoder ResNet18 will produce (Batch, 512, 1, 1)
+        # x = self.patch_embedder(x)  # (n_samples, hidden_dim, n_patches ** (1/2), n_patches ** (1/2))
+        # x = einops.rearrange(x, "n c h w -> n (h w) c")  # (n_samples, n_patches, hidden_dim)
+        #x = (subvideos, frames"vectors", Encoder_out_features)
+        for mixer_block in self.blocks:
+            x = mixer_block(x)  # (n_samples, n_patches, hidden_dim) is equivlant to (n_subvideos, n_frames, hidden_dim)
+
+        x = self.pre_head_norm(x)  # (n_subvideos, n_frames, hidden_dim)
+        x = x.mean(dim=1)  # (n_subvideos, hidden_dim) global average pooling in the original paper
+
+        return x
+
+class ResNet_subVideo_MLP(nn.Module):
+    def __init__(self, num_classes=4, pretrained=False, resnet50=True,
+                 feature_extract=False, Encoder_CheckPoint=None):
+        super(ResNet_subVideo_MLP, self).__init__()
+        # Our 2D encoder, We can consider 3D encoder instead
+        self.SubVideo_Encoder = SubVideo_Encoder(num_classes=num_classes, pretrained=pretrained, resnet50=resnet50,
+                                        feature_extract=feature_extract,Encoder_CheckPoint=Encoder_CheckPoint)
+        self.encoder_out_features = self.SubVideo_Encoder.Encoder_out_features # probabily 2048
+        self.normGRU = nn.BatchNorm1d(self.encoder_out_features)
+        # I am expecting 25 vectors for each sub-video. This line needs to be changed if num of images per subvideo change
+        self.Mlp = Mlp(n_frames=25, encoder_features= self.encoder_out_features )
+
+        # (vectore from sequence + vector from non-sequence) = encoder_out_features*2
+        self.fc = nn.Linear(self.encoder_out_features*2, num_classes)
+
+
+    def forward(self, x):
+        # *************this is default code*************
+
+        output_dic = self.SubVideo_Encoder(x)
+        x = output_dic["x"]  # x=(subvideos, frames"vectors", Encoder_out_features)
+        x_shape = x.shape
+        x_gru = output_dic["x_gru"]  # x_gru = (subvideos, Encoder_out_features)
+        x_gru = self.normGRU(x_gru)
+
+        # ************ your non-sequence code ********************
+        x_Mlp = self.Mlp(x)
+
+        # *************** this is default code********************
+        x_cat = torch.cat((x_Mlp, x_gru), dim=1)  # -> (subvideos, Encoder_out_features*2)
+
+        output = self.fc(x_cat)  # -> (subvideos, 4)
+
+        return output
 
 class ResNet_subVideo_FcVert(nn.Module):
     def __init__(self, num_classes=4, pretrained=False, resnet50=True,
